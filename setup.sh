f12d3f26c5e36f57bff3c7416e39d480172d0f57
pss2 <pushkar.sathe@nist.vov>
2024-06-10 17:50:57 -0400

    setup update:    1. Updated gitignore to stop tracking data    2. Updated dvc_tutorial.ipynb to show cmd approach instead of python.    3. Updated environment.yaml    4. Updated setup.sh to try two approaches for activating envirionment.

--- setup.sh
+++ setup.sh
@@ -5,9 +5,18 @@ cd "$REPO_ROOT"
 
 conda env create -f binder/environment.yaml
 
-conda activate dvc-tutorial
+if conda activate dvc-tutoriall; then
+  echo "Activated environment: conda"
+else
+  if source activate dvc-tutorial; then
+    echo "Activated environment: source"
+  else
+    echo "Failed to activate environment"
+    exit 1
+  fi
+fi
+
 bash binder/postBuild
 conda deactivate
 
 chmod +x binder/postBuild
-
